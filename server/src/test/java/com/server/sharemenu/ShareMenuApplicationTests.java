e239c3dd88aad92ac115fe6f56160c867ea74760
Lati12 <latincho2005@gmail.com>
2022-11-26 23:53:48 +0200

    init

---
+++ server/src/test/java/com/server/sharemenu/ShareMenuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.server.sharemenu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShareMenuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
