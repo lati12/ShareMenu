b266e78afc19b148ab9ae6d8c63836e5ec5e3615
Lati12 <latincho2005@gmail.com>
2022-12-04 14:50:25 +0200

    Controllers it's done

--- server/src/main/java/com/server/sharemenu/repositories/EntitylineRepository.java
+++ server/src/main/java/com/server/sharemenu/repositories/EntitylineRepository.java
@@ -1,9 +1,9 @@
 package com.server.sharemenu.repositories;
 
-import com.server.sharemenu.models.Entityline;
+import com.server.sharemenu.models.EntityLine;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EntitylineRepository extends JpaRepository<Entityline, Long> {
+public interface EntitylineRepository extends JpaRepository<EntityLine, Long> {
 }
