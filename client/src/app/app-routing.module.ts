846f30ffdf9078dbbdd9986e0644545972cc55d6
Lati12 <latincho2005@gmail.com>
2022-12-28 20:28:22 +0200

    wip

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {EntitylineComponent} from "./components/entityline/entityline.component";
+import {SocialproviderComponent} from "./components/socialprovider/socialprovider.component";
+import {TemplateComponent} from "./components/template/template.component";
+import {UsersComponent} from "./components/users/users.component";
+import {SharemenuComponent} from "./components/sharemenu/sharemenu.component";
+import {ItemComponent} from "./components/item/item.component";
+import {ItemCategoryComponent} from "./components/item-category/item-category.component";
+import {EntityheaderComponent} from "./components/entityheader/entityheader.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'entityheader', component:EntityheaderComponent},
+  {path: 'entityline',component:EntitylineComponent },
+  {path: 'socialprovider', component:SocialproviderComponent},
+  {path: 'template', component: TemplateComponent},
+  {path: 'users', component: UsersComponent},
+  {path: 'sharemenu', component: SharemenuComponent},
+  {path: 'item', component: ItemComponent},
+  {path: 'itemcategory', component: ItemCategoryComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- client/src/app/app.component.spec.ts
+++ client/src/app/app.component.spec.ts
@@ -20,16 +20,15 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ShareMenu'`, () => {
+  it(`should have as title 'ShareRestaurant'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ShareMenu');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('ShareMenu app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('ShareRestaurant app is running!');
   });
 });
