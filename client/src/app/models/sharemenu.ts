147a88b5d63ad349bde85bc52c5c088edb45a931
Lati12 <latincho2005@gmail.com>
2022-12-28 20:55:12 +0200

    wip

--- client/src/app/models/sharemenu.ts
+++ client/src/app/models/sharemenu.ts
@@ -1,8 +1,8 @@
-import {Entityheader} from "./entityheader";
-import {Socialproviders} from "./socialproviders";
+//import {Entityheader} from "./entityheader";
+//import {Socialproviders} from "./socialproviders";
 
 export class  Sharemenu{
   id:number = 0;
-  entityheaderid: Entityheader = new Entityheader();
-  socialprovider: Socialproviders = new Socialproviders();
+  //entityheaderid: Entityheader = new Entityheader();
+  //socialprovider: Socialproviders = new Socialproviders();
 }
